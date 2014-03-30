38e626da7fa0db62c16dee4eb783fff2ce952378
Joey Hess <joey@kitenet.net>
2014-03-30 00:33:45 -0400

    improve

--- Property/Hostname.hs
+++ Property/Hostname.hs
@@ -6,7 +6,6 @@ import Utility.SafeCommand
 type HostName = String
 
 set :: HostName -> Property
-set hostname = combineProperties ("hostname " ++ hostname)
-	[ fileHasContent "/etc/hostname" [hostname]
-	, cmdProperty "hostname" [Param hostname]
-	]
+set hostname = 
+	fileHasContent "/etc/hostname" [hostname]
+		`onChange` cmdProperty "hostname" [Param hostname]
