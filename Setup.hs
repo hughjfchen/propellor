8f2ac23b41c6bbc24c658831a6a988c0d23a9f7d
Joey Hess <joey@kitenet.net>
2014-03-30 23:39:07 -0400

    fix

--- Setup.hs
+++ Setup.hs
@@ -1,14 +1,5 @@
 {- cabal setup file -}
 
 import Distribution.Simple
-import Distribution.Simple.Setup
 
-import qualified Build.Configure as Configure
-
-main = defaultMainWithHooks simpleUserHooks
-	{ preConf = configure
-	}
-
-configure _ _ = do
-	Configure.run Configure.tests
-	return (Nothing, [])
+main = defaultMain
