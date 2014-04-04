f75db890075571b45b88b62e14b15cf85ad90cdc
Joey Hess <joey@kitenet.net>
2014-04-04 01:16:16 -0400

    improve

--- Propellor/Config/Simple.hs
+++ Propellor/Config/Simple.hs
@@ -1,17 +1,20 @@
 -- | This is the main configuration file for Propellor, and is used to build
 -- the propellor program.
 
+module Propellor.Config.Simple where
+
 import Propellor
 import Propellor.CmdLine
 import qualified Propellor.Property.File as File
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.Network as Network
-import qualified Propellor.Property.Ssh as Ssh
+--import qualified Propellor.Property.Ssh as Ssh
 import qualified Propellor.Property.Cron as Cron
-import qualified Propellor.Property.Sudo as Sudo
+--import qualified Propellor.Property.Sudo as Sudo
 import qualified Propellor.Property.User as User
-import qualified Propellor.Property.Hostname as Hostname
-import qualified Propellor.Property.Reboot as Reboot
+--import qualified Propellor.Property.Hostname as Hostname
+--import qualified Propellor.Property.Reboot as Reboot
+--import qualified Propellor.Property.Tor as Tor
 import qualified Propellor.Property.Docker as Docker
 
 main :: IO ()
