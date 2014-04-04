ff49cf562c3761993a22ea6fe6904325a1e53d33
Joey Hess <joey@kitenet.net>
2014-04-03 23:32:00 -0400

    propellor spin

--- Propellor/Property/Hostname.hs
+++ Propellor/Property/Hostname.hs
@@ -3,6 +3,9 @@ module Propellor.Property.Hostname where
 import Propellor
 import qualified Propellor.Property.File as File
 
+-- | Sets the hostname. Should be provided with a FQDN, and will configure
+-- both /etc/hostname (with the base hostname) and /etc/hosts (with the
+-- full hostname). Also sets the current hostname.
 set :: HostName -> Property
 set hostname = "/etc/hostname" `File.hasContent` [hostname]
 	`onChange` cmdProperty "hostname" [hostname]
