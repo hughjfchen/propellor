335cce275afb78931416748c9e9acabb22c68f64
Joey Hess <joey@kitenet.net>
2014-04-09 00:57:57 -0400

    propellor spin

--- Propellor/Property/Scheduled.hs
+++ Propellor/Property/Scheduled.hs
@@ -20,6 +20,7 @@ import qualified Data.Map as M
 period :: Property -> Recurrance -> Property
 period prop recurrance = Property desc $ do
 	lasttime <- getLastChecked (propertyDesc prop)
+	print lasttime
 	nexttime <- fmap startTime <$> nextTime schedule lasttime
 	t <- localNow
 	if Just t >= nexttime
