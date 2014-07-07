1a7c10d4604a5046617d337c1aa1a77c0a0cf7d2
Joey Hess <joey@kitenet.net>
2014-07-07 02:26:03 -0400

    propellor spin

--- config-joey.hs
+++ config-joey.hs
@@ -72,7 +72,7 @@ hosts =                 --                  (o)  `
 		& Apt.buildDep ["git-annex"] `period` Daily
 	
 	-- Main kite server.
-  	, standardSystem "kite.kitenet.net" Stable "amd64"
+  	, standardSystem "kite.kitenet.net" Unstable "amd64"
 	  	& ipv4 "66.228.36.95"
 		& ipv6 "2600:3c03::f03c:91ff:fe73:b0d2"
 
