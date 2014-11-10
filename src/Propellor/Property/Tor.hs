053183d44bea516861fccefeb07a4ea4d90e5fcb
Félix Sipma <felix.sipma@no-log.org>
2014-11-10 20:39:15 +0100

    Tor: hidden service
    Signed-off-by: Félix Sipma <felix.sipma@no-log.org>

--- src/Propellor/Property/Tor.hs
+++ src/Propellor/Property/Tor.hs
@@ -16,5 +16,28 @@ isBridge = setup `requires` Apt.installed ["tor"]
 		, "Exitpolicy reject *:*"
 		] `onChange` restarted
 
+hiddenServiceAvailable :: FilePath -> Int -> Property
+hiddenServiceAvailable dir port = hiddenServiceHostName prop
+  where
+	prop = "/etc/tor/torrc" `File.containsLines`
+			[ "HiddenServiceDir " ++ dir
+			, "HiddenServicePort " ++ show port ++ " 127.0.0.1:" ++ show port
+			]
+			`describe` "hidden service available"
+			`onChange` Service.reloaded "tor"
+	hiddenServiceHostName p =  adjustProperty p $ \satisfy -> do
+		r <- satisfy
+		h <- liftIO $ readFile (dir </> "hostname")
+		warningMessage $ unlines ["hidden service hostname:", h]
+		return r
+
+hiddenService :: FilePath -> Int -> Property
+hiddenService dir port = "/etc/tor/torrc" `File.containsLines`
+	[ "HiddenServiceDir " ++ dir
+	, "HiddenServicePort " ++ show port ++ " 127.0.0.1:" ++ show port
+	]
+	`describe` ("hidden service (" ++ dir ++ " " ++ show port ++ ") available")
+	`onChange` Service.reloaded "tor"
+
 restarted :: Property
 restarted = Service.restarted "tor"
