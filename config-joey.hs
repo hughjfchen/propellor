492c2b98a5e001c5ad3d99710c2540b4b95642df
Joey Hess <joey@kitenet.net>
2014-11-19 00:39:35 -0400

    propellor spin

--- config-joey.hs
+++ config-joey.hs
@@ -53,7 +53,7 @@ darkstar = host "darkstar.kitenet.net"
 	& Apt.buildDep ["git-annex"] `period` Daily
 	& Docker.configured
 	! Docker.docked hosts "android-git-annex"
-	! Docker.docked hosts "simple-debian"
+	& Docker.docked hosts "simple-debian"
 
 clam :: Host
 clam = standardSystem "clam.kitenet.net" Unstable "amd64"
