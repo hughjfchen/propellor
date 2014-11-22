6be49197f6ddf391a21b61e0996ef4bb75cd8b1b
Joey Hess <joey@kitenet.net>
2014-11-21 20:09:33 -0400

    allow configuring systemd-nspawn parameters

--- src/Propellor/Types/Chroot.hs
+++ src/Propellor/Types/Chroot.hs
@@ -3,13 +3,27 @@ module Propellor.Types.Chroot where
 import Data.Monoid
 import qualified Data.Map as M
 
-data ChrootInfo h = ChrootInfo
-	{ _chroots :: M.Map FilePath h
+data ChrootInfo host = ChrootInfo
+	{ _chroots :: M.Map FilePath host
+	, _chrootCfg :: ChrootCfg
 	}
 	deriving (Show)
 
-instance Monoid (ChrootInfo h) where
-	mempty = ChrootInfo mempty
+instance Monoid (ChrootInfo host) where
+	mempty = ChrootInfo mempty mempty
 	mappend old new = ChrootInfo
 		{ _chroots = M.union (_chroots old) (_chroots new)
+		, _chrootCfg = _chrootCfg old <> _chrootCfg new
 		}
+
+data ChrootCfg
+	= ChrootCfg
+	| SystemdNspawnCfg [String]
+	deriving (Show)
+
+instance Monoid ChrootCfg where
+	mempty = ChrootCfg
+	mappend _ ChrootCfg = ChrootCfg
+	mappend ChrootCfg r = r
+	mappend (SystemdNspawnCfg l1) (SystemdNspawnCfg l2) =
+		SystemdNspawnCfg (l1 <> l2)
