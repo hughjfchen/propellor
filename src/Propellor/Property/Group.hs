6aa21366d57679f82fbaf96a90f93d2d1ac7f223
Félix Sipma <felix.sipma@no-log.org>
2014-11-23 18:33:26 +0100

    Group properties
    Signed-off-by: Félix Sipma <felix.sipma@no-log.org>

---
+++ src/Propellor/Property/Group.hs
@@ -0,0 +1,14 @@
+module Propellor.Property.Group where
+
+import Propellor
+
+type GID = Int
+
+exists :: GroupName -> Maybe GID -> Property
+exists group' mgid = check test (cmdProperty "addgroup" $ args mgid)
+	`describe` unwords ["group", group']
+  where
+	groupFile = "/etc/group"
+	test = not <$> elem group' <$> words <$> readProcess "cut" ["-d:", "-f1", groupFile]
+	args Nothing = [group']
+	args (Just gid) = ["--gid", show gid, group']
