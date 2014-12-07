42a0c832483296fb111279fc3512a3dfd44f2089
Joey Hess <joeyh@joeyh.name>
2014-12-07 17:09:55 -0400

    Display a warning when ensureProperty is used on a property which has Info and is so prevented from propigating it.
    Would much rather a type-based fixed, but this is all I have for now.

--- src/Propellor/Types/Docker.hs
+++ src/Propellor/Types/Docker.hs
@@ -1,6 +1,7 @@
 module Propellor.Types.Docker where
 
 import Propellor.Types.OS
+import Propellor.Types.Empty
 
 import Data.Monoid
 import qualified Data.Map as M
@@ -17,6 +18,12 @@ instance Monoid (DockerInfo h) where
 		{ _dockerRunParams = _dockerRunParams old <> _dockerRunParams new
 		, _dockerContainers = M.union (_dockerContainers old) (_dockerContainers new)
 		}
+
+instance Empty (DockerInfo h) where
+	isEmpty i = and
+		[ isEmpty (_dockerRunParams i)
+		, isEmpty (_dockerContainers i)
+		]
 
 newtype DockerRunParam = DockerRunParam (HostName -> String)
 
