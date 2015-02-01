f276466cf280b9ce91bbfefce35d2a27ebc87843
Joey Hess <joeyh@joeyh.name>
2015-02-01 13:36:58 -0400

    propellor spin

--- src/Propellor/Property/SiteSpecific/JoeySites.hs
+++ src/Propellor/Property/SiteSpecific/JoeySites.hs
@@ -516,6 +516,8 @@ kiteMailServer = propertyList "kitenet.net mail server" $ props
 
 		, "# Password auth for relaying"
 		, "smtpd_sasl_auth_enable = yes"
+		, "smtpd_sasl_type = dovecot"
+		, "smtpd_sasl_path = private/auth"
 		, "smtpd_sasl_security_options = noanonymous"
 		, "smtpd_sasl_local_domain = kitenet.net"
 
