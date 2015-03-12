93e750fc83d0577351b7a240499686dd98b44dbb
Joey Hess <joeyh@joeyh.name>
2015-03-12 19:41:35 -0400

    propellor spin

--- src/Propellor/Property/SiteSpecific/JoeySites.hs
+++ src/Propellor/Property/SiteSpecific/JoeySites.hs
@@ -48,12 +48,13 @@ scrollBox = propertyList "scroll server" $ props
 		, "rm -f \"$t\""
 		, "mkdir \"$t\""
 		, "cd \"$t\""
+		, "echo"
+		, "echo Press Enter to start the game."
+		, "read me"
 		, "SHELL=/bin/sh script --timing=timing -c " ++ g
 		] `onChange` (s `File.mode` (combineModes (ownerWriteMode:readModes ++ executeModes)))
 	& g `File.hasContent`
 		[ "#!/bin/sh"
-		, "echo Press Enter to start the game."
-		, "read me"
 		, "if ! ../../scroll/scroll; then"
 		, "echo Scroll seems to have ended unexpectedly. Possibly a bug.."
 		, "else"
