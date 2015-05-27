a230b2a837425f7eae14cabdb05c4c1223de4f86
Joey Hess <joeyh@joeyh.name>
2015-05-27 19:46:18 -0400

    propellor spin

--- config-joey.hs
+++ config-joey.hs
@@ -132,12 +132,12 @@ orca = standardSystem "orca.kitenet.net" Unstable "amd64"
 	& Systemd.persistentJournal
 	& Docker.configured
 	! Docker.docked (GitAnnexBuilder.standardAutoBuilderContainer dockerImage "amd64" 15 "2h")
+	! Docker.docked (GitAnnexBuilder.standardAutoBuilderContainer dockerImage "i386" 45 "2h")
+	& Docker.garbageCollected -- `period` Daily
 	& Systemd.nspawned (GitAnnexBuilder.standardAutoBuilderContainerNspawn "amd64" 15 "2h")
-	& Docker.docked (GitAnnexBuilder.standardAutoBuilderContainer dockerImage "i386" 45 "2h")
 	& Docker.docked (GitAnnexBuilder.armelCompanionContainer dockerImage)
 	& Docker.docked (GitAnnexBuilder.armelAutoBuilderContainer dockerImage (Cron.Times "1 3 * * *") "5h")
 	& Docker.docked (GitAnnexBuilder.androidAutoBuilderContainer dockerImage (Cron.Times "1 1 * * *") "3h")
-	& Docker.garbageCollected -- `period` Daily
 	& Apt.buildDep ["git-annex"] `period` Daily
 
 -- This is not a complete description of kite, since it's a
