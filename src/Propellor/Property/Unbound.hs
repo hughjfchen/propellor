19834cdd911629876e173cbd2e8e4889f117e7ed
Félix Sipma <felix.sipma@no-log.org>
2015-09-16 22:39:37 +0200

    Unbound: cachingServer property
    Signed-off-by: Félix Sipma <felix.sipma@no-log.org>

--- src/Propellor/Property/Unbound.hs
+++ src/Propellor/Property/Unbound.hs
@@ -4,18 +4,32 @@ module Propellor.Property.Unbound
 	( installed
 	, restarted
 	, reloaded
-	, genAddressNoTtl
-	, genAddress
-	, genMX
-	, genPTR
-	, genZoneStatic
-	, genZoneTransparent
+	, cachingDnsServer
 	) where
 
 import Propellor
+import Propellor.Property.File
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.Service as Service
 
+import Data.List (find)
+
+
+type ConfSection = String
+
+type UnboundSetting = (UnboundKey, UnboundValue)
+
+type UnboundSection = (ConfSection, [UnboundSetting])
+
+type UnboundZone = (BindDomain, ZoneType)
+
+type UnboundHost = (BindDomain, Record)
+
+type UnboundKey = String
+
+type UnboundValue = String
+
+type ZoneType = String
 
 installed :: Property NoInfo
 installed = Apt.installed ["unbound"]
@@ -31,6 +45,45 @@ dValue (RelDomain d) = d
 dValue (AbsDomain d) = d ++ "."
 dValue (RootDomain) = "@"
 
+sectionHeader :: ConfSection -> String
+sectionHeader header = header ++ ":"
+
+config :: FilePath
+config = "/etc/unbound/unbound.conf.d/propellor.conf"
+
+cachingDnsServer :: [UnboundSection] -> [UnboundZone] -> [UnboundHost] -> Property NoInfo
+cachingDnsServer sections zones hosts =
+	config `hasContent` (comment : otherSections ++ serverSection)
+  where
+	comment = "# deployed with propellor, do not modify"
+	serverSection = genSection (fromMaybe ("server", []) $ find ((== "server") . fst) sections)
+		++ map genZone zones
+		++ map (uncurry genRecord') hosts
+	otherSections = foldr ((++) . genSection) [] sections
+
+genSection :: UnboundSection -> [Line]
+genSection (section, settings) = sectionHeader section : map genSetting settings
+
+genSetting :: UnboundSetting -> Line
+genSetting (key, value) = "    " ++ key ++ ": " ++ value
+
+genZone :: UnboundZone -> Line
+genZone (dom, zt) = "    local-zone: \"" ++ dValue dom ++ "\" " ++ zt
+
+genRecord' :: BindDomain -> Record -> Line
+genRecord' dom r = "    local-data: \"" ++ fromMaybe "" (genRecord dom r) ++ "\""
+
+genRecord :: BindDomain -> Record -> Maybe String
+genRecord dom (Address addr) = Just $ genAddressNoTtl dom addr
+genRecord dom (MX priority dest) = Just $ genMX dom priority dest
+genRecord dom (PTR revip) = Just $ genPTR dom revip
+genRecord _ (CNAME _) = Nothing
+genRecord _ (NS _) = Nothing
+genRecord _ (TXT _) = Nothing
+genRecord _ (SRV _ _ _ _) = Nothing
+genRecord _ (SSHFP _ _ _) = Nothing
+genRecord _ (INCLUDE _) = Nothing
+
 genAddressNoTtl :: BindDomain -> IPAddr -> String
 genAddressNoTtl dom = genAddress dom Nothing
 
@@ -40,22 +93,10 @@ genAddress dom ttl addr = case addr of
 	IPv6 _ -> genAddress' "AAAA" dom ttl addr
 
 genAddress' :: String -> BindDomain -> Maybe Int -> IPAddr -> String
-genAddress' recordtype dom ttl addr = localData $ dValue dom ++ " " ++ maybe "" (\ttl' -> show ttl' ++ " ") ttl ++ "IN " ++ recordtype ++ " " ++ fromIPAddr addr
+genAddress' recordtype dom ttl addr = dValue dom ++ " " ++ maybe "" (\ttl' -> show ttl' ++ " ") ttl ++ "IN " ++ recordtype ++ " " ++ fromIPAddr addr
 
-genMX :: BindDomain -> BindDomain -> Int -> String
-genMX dom dest priority = localData $ dValue dom ++ " " ++ "MX" ++ " " ++ show priority ++ " " ++ dValue dest
+genMX :: BindDomain -> Int -> BindDomain -> String
+genMX dom priority dest = dValue dom ++ " " ++ "MX" ++ " " ++ show priority ++ " " ++ dValue dest
 
-genPTR :: BindDomain -> IPAddr -> String
-genPTR dom ip = localData $ reverseIP ip ++ ". " ++ "PTR" ++ " " ++ dValue dom
-
-localData :: String -> String
-localData conf = "    local-data: \"" ++ conf ++ "\""
-
-genZoneStatic :: BindDomain -> String
-genZoneStatic dom = localZone (dValue dom) "static"
-
-genZoneTransparent :: BindDomain -> String
-genZoneTransparent dom = localZone (dValue dom) "transparent"
-
-localZone :: String -> String -> String
-localZone zone confzone = "    local-zone: \"" ++ zone ++ "\" " ++ confzone
+genPTR :: BindDomain -> ReverseIP -> String
+genPTR dom revip = revip ++ ". " ++ "PTR" ++ " " ++ dValue dom
