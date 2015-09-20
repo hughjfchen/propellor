ca5f973c2b745b75da57d4b3953d50604165fcd0
Joey Hess <joeyh@joeyh.name>
2015-09-20 14:37:41 -0400

    reformat and fix warning about 2 definitions of 'u'

--- src/Propellor/Property/Aiccu.hs
+++ src/Propellor/Property/Aiccu.hs
@@ -7,21 +7,23 @@ confPath :: FilePath
 confPath = "/etc/aiccu.conf"
 
 config :: String -> String -> PrivData -> [String]
-config u t p = [ "protocol tic"
-	       , "server tic.sixxs.net"
-	       , "username " ++ u
-	       , "password " ++ (privDataVal p)
-	       , "ipv6_interface sixxs"
-	       , "tunnel_id " ++ t
-	       , "daemonize true"
-	       , "automatic true"
-	       , "requiretls true"
-	       , "makebeats true"
-	       ]
+config u t p = 
+	[ "protocol tic"
+	, "server tic.sixxs.net"
+	, "username " ++ u
+	, "password " ++ (privDataVal p)
+	, "ipv6_interface sixxs"
+	, "tunnel_id " ++ t
+	, "daemonize true"
+	, "automatic true"
+	, "requiretls true"
+	, "makebeats true"
+	]
 
 hasConfig :: String -> String -> Property HasInfo
 hasConfig t u = withSomePrivData [(Password (u++"/"++t)), (Password u)] (Context "aiccu") $ property "aiccu configured" . writeConfig
-	where writeConfig :: (((PrivDataField, PrivData) -> Propellor Result) -> Propellor Result) -> Propellor Result
-	      writeConfig getpassword = getpassword $ go
-	      go (Password u, p) = makeChange $ writeFile confPath (unlines $ config u t p)
-	      go (f, _) = error $ "Unexpected type of privdata: " ++ show f
+  where
+	writeConfig :: (((PrivDataField, PrivData) -> Propellor Result) -> Propellor Result) -> Propellor Result
+	writeConfig getpassword = getpassword $ go
+	go (Password u', p) = makeChange $ writeFile confPath (unlines $ config u' t p)
+	go (f, _) = error $ "Unexpected type of privdata: " ++ show f
