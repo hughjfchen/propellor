644ce3f6e8876ee4bbecba6d1bf5b74a612d82e4
Joey Hess <joeyh@joeyh.name>
2015-10-28 15:41:26 -0400

    work around waitForProcess race condition
    https://github.com/haskell/process/issues/46

--- src/Utility/Process/Shim.hs
+++ src/Utility/Process/Shim.hs
@@ -1,8 +1,12 @@
-module Utility.Process.Shim (module X, createProcess) where
+module Utility.Process.Shim (module X, createProcess, waitForProcess) where
 
-import System.Process as X hiding (createProcess)
-import Utility.ConcurrentOutput (createProcessConcurrent)
+import System.Process as X hiding (createProcess, waitForProcess)
+import Utility.ConcurrentOutput (createProcessConcurrent, waitForProcessConcurrent)
 import System.IO
+import System.Exit
 
 createProcess :: CreateProcess -> IO (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle) 
 createProcess = createProcessConcurrent
+
+waitForProcess :: ProcessHandle -> IO ExitCode
+waitForProcess = waitForProcessConcurrent
