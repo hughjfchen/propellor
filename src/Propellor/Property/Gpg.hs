f77badde96531d3723c633084dd82597214d0f38
Félix Sipma <felix.sipma@no-log.org>
2015-12-14 09:24:12 +0100

    Gpg: use isJust
    (cherry picked from commit 1bb2377598e447e81e20a0e1de1a5d466851d655)

--- src/Propellor/Property/Gpg.hs
+++ src/Propellor/Property/Gpg.hs
@@ -45,13 +45,9 @@ dotDir (User u) = do
 hasPrivKey :: GpgKeyId -> User -> IO Bool
 hasPrivKey (GpgKeyId keyid) (User u) = do
 	value <- catchMaybeIO $ readProcess "su" ["-c", "gpg --list-secret-keys " ++ keyid, u]
-	return $ case value of
-		Just _ -> True
-		_ -> False
+	return $ isJust value
 
 hasPubKey :: GpgKeyId -> User -> IO Bool
 hasPubKey (GpgKeyId keyid) (User u) = do
 	value <- catchMaybeIO $ readProcess "su" ["-c", "gpg --list-public-keys " ++ keyid, u]
-	return $ case value of
-		Just _ -> True
-		_ -> False
+	return $ isJust value
