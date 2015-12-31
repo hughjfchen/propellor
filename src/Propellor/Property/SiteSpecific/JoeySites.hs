5e73aa446e46b61ebea77af5a6addaa8be517c1d
Joey Hess <joeyh@joeyh.name>
2015-12-31 12:19:46 -0400

    propellor spin

--- src/Propellor/Property/SiteSpecific/JoeySites.hs
+++ src/Propellor/Property/SiteSpecific/JoeySites.hs
@@ -702,7 +702,7 @@ kiteMailServer = propertyList "kitenet.net mail server" $ props
 	dovecotusers = "/etc/dovecot/users"
 
 	ssmtp = Postfix.Service 
-		(Postfix.InetService (Nothing, "ssmtp"))
+		(Postfix.InetService Nothing "ssmtp")
 		"smtpd" Postfix.defServiceOpts
 
 -- Configures postfix to relay outgoing mail to kitenet.net, with
