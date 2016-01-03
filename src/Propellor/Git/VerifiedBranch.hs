f86804fa27a2cf5b1972b14ab41e81edb85ad661
Joey Hess <joeyh@joeyh.name>
2016-01-03 16:56:00 -0400

    refactor into smaller modules to untangle git and gpg modules

---
+++ src/Propellor/Git/VerifiedBranch.hs
@@ -0,0 +1,51 @@
+module Propellor.Git.VerifiedBranch where
+
+import Propellor.Base
+import Propellor.Git
+import Propellor.Gpg
+import Propellor.PrivData.Paths
+import Utility.FileMode
+
+{- To verify origin branch commit's signature, have to convince gpg
+ - to use our keyring.
+ - While running git log. Which has no way to pass options to gpg.
+ - Argh!
+ -}
+verifyOriginBranch :: String -> IO Bool
+verifyOriginBranch originbranch = do
+	let gpgconf = privDataDir </> "gpg.conf"
+	writeFile gpgconf $ unlines
+		[ " keyring " ++ keyring
+		, "no-auto-check-trustdb"
+		]
+	-- gpg is picky about perms
+	modifyFileMode privDataDir (removeModes otherGroupModes)
+	s <- readProcessEnv "git" ["log", "-n", "1", "--format=%G?", originbranch]
+		(Just [("GNUPGHOME", privDataDir)])
+	nukeFile $ privDataDir </> "trustdb.gpg"
+	nukeFile $ privDataDir </> "pubring.gpg"
+	nukeFile $ privDataDir </> "gpg.conf"
+	return (s == "U\n" || s == "G\n")
+
+-- Returns True if HEAD is changed by fetching and merging from origin.
+fetchOrigin :: IO Bool
+fetchOrigin = do
+	branchref <- getCurrentBranch
+	let originbranch = "origin" </> branchref
+
+	void $ actionMessage "Pull from central git repository" $
+		boolSystem "git" [Param "fetch"]
+
+	oldsha <- getCurrentGitSha1 branchref
+
+	whenM (doesFileExist keyring) $
+		ifM (verifyOriginBranch originbranch)
+			( do
+				putStrLn $ "git branch " ++ originbranch ++ " gpg signature verified; merging"
+				hFlush stdout
+				void $ boolSystem "git" [Param "merge", Param originbranch]
+			, warningMessage $ "git branch " ++ originbranch ++ " is not signed with a trusted gpg key; refusing to deploy it! (Running with previous configuration instead.)"
+			)
+
+	newsha <- getCurrentGitSha1 branchref
+	return $ oldsha /= newsha
--- src/Propellor/Gpg.hs
+++ src/Propellor/Gpg.hs
@@ -13,7 +13,7 @@ import Prelude
 
 import Propellor.PrivData.Paths
 import Propellor.Message
-import Utility.Exception
+import Propellor.Git.Config
 import Utility.SafeCommand
 import Utility.Process
 import Utility.Monad
@@ -22,14 +22,14 @@ import Utility.Tmp
 import Utility.FileSystemEncoding
 import Utility.Env
 
+type KeyId = String
+
 getGpgBin :: IO String
 getGpgBin = do
 	gitGpgBin <- getGitConfigValue "gpg.program"
 	case gitGpgBin of
 		Nothing -> getEnvDefault "GNUPGBIN" "gpg"
 		Just b -> return b
-
-type KeyId = String
 
 keyring :: FilePath
 keyring = privDataDir </> "keyring.gpg"
@@ -117,15 +117,6 @@ reencryptPrivData = ifM (doesFileExist privDataFile)
 		gitAdd privDataFile
 	, return True
 	)
-
-getGitConfigValue :: String -> IO (Maybe String)
-getGitConfigValue key = do
-	value <- catchMaybeIO $
-		takeWhile (/= '\n')
-			<$> readProcess "git" ["config", key]
-	return $ case value of
-		Just v | not (null v) -> Just v
-		_ -> Nothing
 
 gitAdd :: FilePath -> IO Bool
 gitAdd f = boolSystem "git"
