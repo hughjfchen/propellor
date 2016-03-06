9e748e9f0d879bd58618f8a9ec48bd1c2e25ab65
Joey Hess <joeyh@joeyh.name>
2016-03-06 15:54:52 -0400

    back

--- src/System/Console/Concurrent/Internal.hs
+++ src/System/Console/Concurrent/Internal.hs
@@ -297,7 +297,7 @@ fgProcess p = do
 	return (toConcurrentProcessHandle r)
 	
 debug :: [String] -> IO ()
-debug = hPutStrLn stderr . unwords
+debug = debugM "concurrent-output" . unwords
 
 showProc :: P.CreateProcess -> String
 showProc = go . P.cmdspec
