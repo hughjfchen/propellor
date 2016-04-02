828830eace62dba7d75b656142d83f8396fd2968
Joey Hess <joeyh@joeyh.name>
2016-04-02 01:51:01 -0400

    typo

--- src/wrapper.hs
+++ src/wrapper.hs
@@ -19,6 +19,7 @@ import System.Directory
 import System.Environment (getArgs)
 import System.Exit
 import System.Posix.Directory
+import Control.Monad.IfElse
 
 main :: IO ()
 main = withConcurrentOutput $ go =<< getArgs
