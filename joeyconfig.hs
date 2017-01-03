50d8edf7269cfc2b30f386a23d96fc854039ee51
Joey Hess <joeyh@joeyh.name>
2017-01-03 10:36:22 -0400

    propellor spin

--- joeyconfig.hs
+++ joeyconfig.hs
@@ -50,6 +50,7 @@ hosts =                 --                  (o)  `
 	, mayfly
 	, oyster
 	, orca
+	, baleen
 	, honeybee
 	, kite
 	, elephant
@@ -181,6 +182,16 @@ oyster = host "oyster.kitenet.net" $ props
 	-- that port for ssh, for traveling on bad networks that
 	-- block 22.
 	& Ssh.listenPort (Port 80)
+
+baleen :: Host
+baleen = host "baleen.kitenet.net" $ props
+	& standardSystem Unstable X86_64 [ "New git-annex build box." ]
+	-- Not on public network; ssh access via bounce host.
+	
+	& Apt.unattendedUpgrades
+	& Postfix.satellite
+	& Apt.serviceInstalledRunning "ntp"
+	& Systemd.persistentJournal
 
 orca :: Host
 orca = host "orca.kitenet.net" $ props
