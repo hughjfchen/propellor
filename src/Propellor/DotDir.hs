1b356d9b3c278fe45a60fb297cb50768f2463b0b
Joey Hess <joeyh@joeyh.name>
2017-03-01 16:20:26 -0400

    github mirror of propellor is gone
    * The github mirror of propellor's git repository has been removed,
      since github's terms of service has started imposing unwanted licensing
      requirements.
    * propellor --init: The option to clone propellor's git repository
      used to use the github mirror, and has been changed to use a different
      mirror.

--- src/Propellor/DotDir.hs
+++ src/Propellor/DotDir.hs
@@ -47,10 +47,10 @@ disthead = distdir </> "head"
 upstreambranch :: String
 upstreambranch = "upstream/master"
 
--- Using the github mirror of the main propellor repo because
+-- Using the joeyh.name mirror of the main propellor repo because
 -- it is accessible over https for better security.
 netrepo :: String
-netrepo = "https://github.com/joeyh/propellor.git"
+netrepo = "https://git.joeyh.name/propellor.git"
 
 dotPropellor :: IO FilePath
 dotPropellor = do
