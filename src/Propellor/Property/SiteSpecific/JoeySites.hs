8096511f807f633b6a50631ef03c1db98a910be1
Joey Hess <joeyh@joeyh.name>
2017-04-03 15:41:31 -0400

    avoid opendkim checks on incoming email
    This caused a problem when eg a gmail email was sent to branchable and
    forwarded on to kite. Kite thought branchable was forging the dkim
    signature of gmail, and gmail does strict enforcement of dkim, so it
    rejected it.
    
    DKIM seems like a mightly hefty hammer, and I remember similar issues
    being dicussed where mailing list software got broken by DKIM.
    I guess this will mean a few more joe-jobs get through, but I'd rather
    not silently lose important legitimate email!

--- src/Propellor/Property/SiteSpecific/JoeySites.hs
+++ src/Propellor/Property/SiteSpecific/JoeySites.hs
@@ -573,8 +573,8 @@ kiteMailServer = propertyList "kitenet.net mail server" $ props
 		, "# Enable postgrey."
 		, "smtpd_recipient_restrictions = permit_tls_clientcerts,permit_sasl_authenticated,,permit_mynetworks,reject_unauth_destination,check_policy_service inet:127.0.0.1:10023"
 
-		, "# Enable spamass-milter, amavis-milter, opendkim"
-		, "smtpd_milters = unix:/spamass/spamass.sock unix:amavis/amavis.sock inet:localhost:8891"
+		, "# Enable spamass-milter, amavis-milter (opendkim is not enabled because it causes mails forwarded from eg gmail to be rejected)"
+		, "smtpd_milters = unix:/spamass/spamass.sock unix:amavis/amavis.sock"
 		, "# opendkim is used for outgoing mail"
 		, "non_smtpd_milters = inet:localhost:8891"
 		, "milter_connect_macros = j {daemon_name} v {if_name} _"
