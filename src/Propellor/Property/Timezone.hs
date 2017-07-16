6f2ea4ecc79dc191ec690d57d0cabb19542ddd65
Sean Whitton <spwhitton@spwhitton.name>
2017-07-15 17:02:48 -0700

    add Timezone.configured

---
+++ src/Propellor/Property/Timezone.hs
@@ -0,0 +1,19 @@
+-- | Maintainer: Sean Whitton <spwhitton@spwhitton.name>
+
+module Propellor.Property.Timezone where
+
+import Propellor.Base
+import qualified Propellor.Property.Apt as Apt
+
+type Timezone = String
+
+-- | Sets the system's timezone
+configured :: Timezone -> Property DebianLike
+configured zone = File.hasContent "/etc/timezone" zone
+	`onChange` update
+	`describe` (zone ++ " timezone configured")
+  where
+	update = Apt.reConfigure "tzdata" mempty
+		-- work around a bug in recent tzdata.  See
+		-- https://bugs.launchpad.net/ubuntu/+source/tzdata/+bug/1554806/
+		`requires` File.notPresent "/etc/localtime"
