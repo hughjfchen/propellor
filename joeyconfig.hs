baa8e44f4b961f43238d153ed5e4e32d2116be71
Joey Hess <joeyh@joeyh.name>
2017-11-17 15:36:04 -0400

    propellor spin

--- joeyconfig.hs
+++ joeyconfig.hs
@@ -95,8 +95,8 @@ darkstar = host "darkstar.kitenet.net" $ props
 	& Ssh.userKeys (User "joey") hostContext
 		[ (SshRsa, "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1YoyHxZwG5Eg0yiMTJLSWJ/+dMM6zZkZiR4JJ0iUfP+tT2bm/lxYompbSqBeiCq+PYcSC67mALxp1vfmdOV//LWlbXfotpxtyxbdTcQbHhdz4num9rJQz1tjsOsxTEheX5jKirFNC5OiKhqwIuNydKWDS9qHGqsKcZQ8p+n1g9Lr3nJVGY7eRRXzw/HopTpwmGmAmb9IXY6DC2k91KReRZAlOrk0287LaK3eCe1z0bu7LYzqqS+w99iXZ/Qs0m9OqAPnHZjWQQ0fN4xn5JQpZSJ7sqO38TBAimM+IHPmy2FTNVVn9zGM+vN1O2xr3l796QmaUG1+XLL0shfR/OZbb joey@darkstar")
 		]
-	& imageBuilt (RawDiskImage "/srv/sheevaplug.img")
-		(hostChroot sheevaplug (Debootstrapped mempty))
+	& imageBuilt (RawDiskImage "/srv/test.img")
+		(hostChroot lime (Debootstrapped mempty))
 		MSDOS
 		[ partition EXT2
 			`mountedAt` "/boot"
