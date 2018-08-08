c7f1d72d466acd85c0a0747053dda29204cb34c8
Joey Hess <joeyh@joeyh.name>
2018-08-08 10:20:41 -0400

    build later in the day
    actually, the container the build runs in has its timezone GMT, so this
    builds at 10:15 local time

--- joeyconfig.hs
+++ joeyconfig.hs
@@ -210,7 +210,7 @@ honeybee = host "honeybee.kitenet.net" $ props
   where
 	autobuilder = Systemd.nspawned $ GitAnnexBuilder.autoBuilderContainer
 		GitAnnexBuilder.armAutoBuilder
-		Unstable ARMEL Nothing (Cron.Times "15 10 * * *") "10h"
+		Unstable ARMEL Nothing (Cron.Times "15 15 * * *") "10h"
 
 -- This is not a complete description of kite, since it's a
 -- multiuser system with eg, user passwords that are not deployed
