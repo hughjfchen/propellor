e62d1775f8124dcd473290b20ed5dc551dcd1103
Joey Hess <joeyh@joeyh.name>
2018-10-24 15:45:05 -0400

    propellor spin

--- src/Propellor/Property/SiteSpecific/GitAnnexBuilder.hs
+++ src/Propellor/Property/SiteSpecific/GitAnnexBuilder.hs
@@ -182,6 +182,7 @@ armAutoBuilder baseautobuilder suite arch flavor =
 	propertyList "arm git-annex autobuilder" $ props
 		& baseautobuilder suite arch flavor
 		-- Works around ghc crash with parallel builds on arm.
+		& File.dirExists (homedir </> ".cabal")
 		& (homedir </> ".cabal" </> "config")
 			`File.containsLine` "jobs: 1"
 		-- Work around https://github.com/systemd/systemd/issues/7135
