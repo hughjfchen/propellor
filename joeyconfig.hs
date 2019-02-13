8d3344513169738dcce37cdebd756976b153f193
Joey Hess <joeyh@joeyh.name>
2019-02-13 17:47:41 -0400

    update

--- joeyconfig.hs
+++ joeyconfig.hs
@@ -71,6 +71,7 @@ darkstar = host "darkstar.kitenet.net" $ props
 	& Laptop.trimSSD
 	& Grub.cmdline_Linux_default "i915.enable_psr=1"
 	! Grub.cmdline_Linux_default "quiet"
+	& User.hasGroup (Use "joey") (Group "dialout")
 
 	& JoeySites.dkimMilter
 	& JoeySites.postfixSaslPasswordClient
