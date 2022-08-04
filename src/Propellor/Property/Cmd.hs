93cabb193ab11da304b3284c92f99620dc54bf38
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-08-04 22:18:15 +0800

    export boolSystem' and safeSystem'

--- src/Propellor/Property/Cmd.hs
+++ src/Propellor/Property/Cmd.hs
@@ -2,7 +2,7 @@
 
 -- | This module lets you construct Properties by running commands and
 -- scripts. To get from an `UncheckedProperty` to a `Property`, it's
--- up to the user to check if the command made a change to the system. 
+-- up to the user to check if the command made a change to the system.
 --
 -- The best approach is to `check` a property, so that the command is only
 -- run when it needs to be. With this method, you avoid running the
@@ -24,37 +24,39 @@
 --
 -- > cmdProperty "service" ["foo", "reload"]
 -- > 	`assume` MadeChange
+module Propellor.Property.Cmd
+  ( -- * Constricting properties running commands and scripts
+    cmdProperty,
+    cmdProperty',
+    cmdPropertyEnv,
+    Script,
+    scriptProperty,
+    userScriptProperty,
+    cmdResult,
 
-module Propellor.Property.Cmd (
-	-- * Constricting properties running commands and scripts
-	cmdProperty,
-	cmdProperty',
-	cmdPropertyEnv,
-	Script,
-	scriptProperty,
-	userScriptProperty,
-	cmdResult,
-	-- * Lower-level interface for running commands
-	CommandParam(..),
-	boolSystem,
-	boolSystemEnv,
-	safeSystem,
-	safeSystemEnv,
-	shellEscape,
-	createProcess,
-	waitForProcess,
-) where
+    -- * Lower-level interface for running commands
+    CommandParam (..),
+    boolSystem,
+    boolSystem',
+    boolSystemEnv,
+    safeSystem,
+    safeSystem',
+    safeSystemEnv,
+    shellEscape,
+    createProcess,
+    waitForProcess,
+  )
+where
 
-import Data.List
-import "mtl" Control.Monad.Reader
 import Control.Applicative
-import Prelude
-
-import Propellor.Types
+import "mtl" Control.Monad.Reader
+import Data.List
 import Propellor.Property
-import Utility.SafeCommand
+import Propellor.Types
 import Utility.Env
-import Utility.Process (createProcess, CreateProcess, waitForProcess)
+import Utility.Process (CreateProcess, createProcess, waitForProcess)
+import Utility.SafeCommand
+import Prelude
 
 -- | A property that can be satisfied by running a command.
 --
@@ -63,10 +65,13 @@ cmdProperty :: String -> [String] -> UncheckedProperty UnixLike
 cmdProperty cmd params = cmdProperty' cmd params id
 
 cmdProperty' :: String -> [String] -> (CreateProcess -> CreateProcess) -> UncheckedProperty UnixLike
-cmdProperty' cmd params mkprocess = unchecked $ property desc $ liftIO $
-	cmdResult <$> boolSystem' cmd (map Param params) mkprocess
+cmdProperty' cmd params mkprocess =
+  unchecked $
+    property desc $
+      liftIO $
+        cmdResult <$> boolSystem' cmd (map Param params) mkprocess
   where
-	desc = unwords $ cmd : params
+    desc = unwords $ cmd : params
 
 cmdResult :: Bool -> Result
 cmdResult False = FailedChange
@@ -76,11 +81,13 @@ cmdResult True = NoChange
 -- with added environment variables in addition to the standard
 -- environment.
 cmdPropertyEnv :: String -> [String] -> [(String, String)] -> UncheckedProperty UnixLike
-cmdPropertyEnv cmd params env = unchecked $ property desc $ liftIO $ do
-	env' <- addEntries env <$> getEnvironment
-	cmdResult <$> boolSystemEnv cmd (map Param params) (Just env')
+cmdPropertyEnv cmd params env = unchecked $
+  property desc $
+    liftIO $ do
+      env' <- addEntries env <$> getEnvironment
+      cmdResult <$> boolSystemEnv cmd (map Param params) (Just env')
   where
-	desc = unwords $ cmd : params
+    desc = unwords $ cmd : params
 
 -- | A series of shell commands. (Without a leading hashbang.)
 type Script = [String]
@@ -89,12 +96,14 @@ type Script = [String]
 scriptProperty :: Script -> UncheckedProperty UnixLike
 scriptProperty script = cmdProperty "sh" ["-c", shellcmd]
   where
-	shellcmd = intercalate " ; " ("set -e" : script)
+    shellcmd = intercalate " ; " ("set -e" : script)
 
 -- | A property that can satisfied by running a script
 -- as user (cd'd to their home directory).
 userScriptProperty :: User -> Script -> UncheckedProperty UnixLike
-userScriptProperty (User user) script = cmdProperty "su"
-	["--login", "--shell", "/bin/sh", "-c", shellcmd, user]
+userScriptProperty (User user) script =
+  cmdProperty
+    "su"
+    ["--login", "--shell", "/bin/sh", "-c", shellcmd, user]
   where
-	shellcmd = intercalate " ; " ("set -e" : "cd" : script)
+    shellcmd = intercalate " ; " ("set -e" : "cd" : script)
--- src/Utility/SafeCommand.hs
+++ src/Utility/SafeCommand.hs
@@ -4,25 +4,26 @@
  -
  - License: BSD-2-clause
  -}
-
 {-# OPTIONS_GHC -fno-warn-tabs #-}
 
 module Utility.SafeCommand where
 
-import System.Exit
-import Utility.Process
-import Utility.Split
-import System.FilePath
+import Control.Applicative
 import Data.Char
 import Data.List
-import Control.Applicative
+import System.Exit
+import System.FilePath
+import Utility.Process
+import Utility.Split
 import Prelude
 
 -- | Parameters that can be passed to a shell command.
 data CommandParam
-	= Param String -- ^ A parameter
-	| File FilePath -- ^ The name of a file
-	deriving (Eq, Show, Ord)
+  = -- | A parameter
+    Param String
+  | -- | The name of a file
+    File FilePath
+  deriving (Eq, Show, Ord)
 
 -- | Used to pass a list of CommandParams to a function that runs
 -- a command and expects Strings. -}
@@ -34,13 +35,13 @@ toCommand' (Param s) = s
 -- Files that start with a non-alphanumeric that is not a path
 -- separator are modified to avoid the command interpreting them as
 -- options or other special constructs.
-toCommand' (File s@(h:_))
-	| isAlphaNum h || h `elem` pathseps = s
-	| otherwise = "./" ++ s
+toCommand' (File s@(h : _))
+  | isAlphaNum h || h `elem` pathseps = s
+  | otherwise = "./" ++ s
   where
-	-- '/' is explicitly included because it's an alternative
-	-- path separator on Windows.
-	pathseps = pathSeparator:"./"
+    -- '/' is explicitly included because it's an alternative
+    -- path separator on Windows.
+    pathseps = pathSeparator : "./"
 toCommand' (File s) = s
 
 -- | Run a system command, and returns True or False if it succeeded or failed.
@@ -53,12 +54,12 @@ boolSystem command params = boolSystem' command params id
 boolSystem' :: FilePath -> [CommandParam] -> (CreateProcess -> CreateProcess) -> IO Bool
 boolSystem' command params mkprocess = dispatch <$> safeSystem' command params mkprocess
   where
-	dispatch ExitSuccess = True
-	dispatch _ = False
+    dispatch ExitSuccess = True
+    dispatch _ = False
 
 boolSystemEnv :: FilePath -> [CommandParam] -> Maybe [(String, String)] -> IO Bool
 boolSystemEnv command params environ = boolSystem' command params $
-	\p -> p { env = environ }
+  \p -> p {env = environ}
 
 -- | Runs a system command, returning the exit status.
 safeSystem :: FilePath -> [CommandParam] -> IO ExitCode
@@ -66,14 +67,14 @@ safeSystem command params = safeSystem' command params id
 
 safeSystem' :: FilePath -> [CommandParam] -> (CreateProcess -> CreateProcess) -> IO ExitCode
 safeSystem' command params mkprocess = do
-	(_, _, _, pid) <- createProcess p
-	waitForProcess pid
+  (_, _, _, pid) <- createProcess p
+  waitForProcess pid
   where
-	p = mkprocess $ proc command (toCommand params)
+    p = mkprocess $ proc command (toCommand params)
 
 safeSystemEnv :: FilePath -> [CommandParam] -> Maybe [(String, String)] -> IO ExitCode
-safeSystemEnv command params environ = safeSystem' command params $ 
-	\p -> p { env = environ }
+safeSystemEnv command params environ = safeSystem' command params $
+  \p -> p {env = environ}
 
 -- | Wraps a shell command line inside sh -c, allowing it to be run in a
 -- login shell that may not support POSIX shell, eg csh.
@@ -87,29 +88,30 @@ shellWrap cmdline = "sh -c " ++ shellEscape cmdline
 shellEscape :: String -> String
 shellEscape f = "'" ++ escaped ++ "'"
   where
-	-- replace ' with '"'"'
-	escaped = intercalate "'\"'\"'" $ splitc '\'' f
+    -- replace ' with '"'"'
+    escaped = intercalate "'\"'\"'" $ splitc '\'' f
 
 -- | Unescapes a set of shellEscaped words or filenames.
 shellUnEscape :: String -> [String]
 shellUnEscape [] = []
 shellUnEscape s = word : shellUnEscape rest
   where
-	(word, rest) = findword "" s
-	findword w [] = (w, "")
-	findword w (c:cs)
-		| c == ' ' = (w, cs)
-		| c == '\'' = inquote c w cs
-		| c == '"' = inquote c w cs
-		| otherwise = findword (w++[c]) cs
-	inquote _ w [] = (w, "")
-	inquote q w (c:cs)
-		| c == q = findword w cs
-		| otherwise = inquote q (w++[c]) cs
+    (word, rest) = findword "" s
+    findword w [] = (w, "")
+    findword w (c : cs)
+      | c == ' ' = (w, cs)
+      | c == '\'' = inquote c w cs
+      | c == '"' = inquote c w cs
+      | otherwise = findword (w ++ [c]) cs
+    inquote _ w [] = (w, "")
+    inquote q w (c : cs)
+      | c == q = findword w cs
+      | otherwise = inquote q (w ++ [c]) cs
 
 -- | For quickcheck.
 prop_isomorphic_shellEscape :: String -> Bool
 prop_isomorphic_shellEscape s = [s] == (shellUnEscape . shellEscape) s
+
 prop_isomorphic_shellEscape_multiword :: [String] -> Bool
 prop_isomorphic_shellEscape_multiword s = s == (shellUnEscape . unwords . map shellEscape) s
 
@@ -123,16 +125,16 @@ segmentXargsOrdered = reverse . map reverse . segmentXargsUnordered
 segmentXargsUnordered :: [FilePath] -> [[FilePath]]
 segmentXargsUnordered l = go l [] 0 []
   where
-	go [] c _ r = (c:r)
-	go (f:fs) c accumlen r
-		| newlen > maxlen && len < maxlen = go (f:fs) [] 0 (c:r)
-		| otherwise = go fs (f:c) newlen r
-	  where
-		len = length f
-		newlen = accumlen + len
+    go [] c _ r = (c : r)
+    go (f : fs) c accumlen r
+      | newlen > maxlen && len < maxlen = go (f : fs) [] 0 (c : r)
+      | otherwise = go fs (f : c) newlen r
+      where
+        len = length f
+        newlen = accumlen + len
 
-	{- 10k of filenames per command, well under 100k limit
-	 - of Linux (and OSX has a similar limit);
-	 - allows room for other parameters etc. Also allows for
-	 - eg, multibyte characters. -}
-	maxlen = 10240
+    {- 10k of filenames per command, well under 100k limit
+    - of Linux (and OSX has a similar limit);
+    - allows room for other parameters etc. Also allows for
+    - eg, multibyte characters. -}
+    maxlen = 10240
